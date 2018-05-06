4c9256635387fa1a338e890e753df08de5d7fe0b
pbeliveau <beliveau.philippe@gmail.com>
2018-05-06 19:32:27 -0400

    Support for Python 3.

--- wb.py
+++ wb.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
-# Import libraries
 
 from sys import argv as arg
-import urllib2 as urllib
+import urllib.request
 import json
 
 # Submit url to wayback machine
 
 inputurl = arg[1] # take in url as argument
 wbkurl = "http://web.archive.org/save/" # wayback machine submission url
-urllib.urlopen(wbkurl+inputurl) # Submit the url
+urllib.request.urlopen(wbkurl+inputurl) # Submit the url
 
 # Return latest snapshot url
 
 wbkav = "http://archive.org/wayback/available?url=" # wayback machine availability api url
-wbcheck = urllib.urlopen(wbkav+inputurl) # open wayback availability url
+wbcheck = urllib.request.urlopen(wbkav+inputurl) # open wayback availability url
 wbcheckjson = json.load(wbcheck) # load json data
 archived_snapshots = wbcheckjson['archived_snapshots']
 latest_snapshot = archived_snapshots['closest']['url']
-print latest_snapshot
+print(latest_snapshot)
